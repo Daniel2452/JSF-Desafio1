14aa727680a5a8f0ea722923cbb5eab76f3ab307
Daniel2452 <daniel.molina.rivera10@gmail.com>
2024-08-15 23:54:46 -0600

    Se agrego manejo de excepcion en el servicio

--- src/main/java/sv/edu/udb/www/rest/ProductService.java
+++ src/main/java/sv/edu/udb/www/rest/ProductService.java
@@ -12,6 +12,9 @@ import javax.ws.rs.core.Response;
 import javax.json.Json;
 import javax.json.JsonArrayBuilder;
 import javax.json.JsonObjectBuilder;
+import javax.ws.rs.core.Response.Status;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Path("/products")
 public class ProductService {
@@ -19,23 +22,37 @@ public class ProductService {
     @Inject
     private ProductBean productBean;
 
+    private static final Logger LOGGER = Logger.getLogger(ProductService.class.getName());
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getProducts() {
-        return Response.ok(productListAsJson()).build();
+        try {
+            return Response.ok(productListAsJson()).build();
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "Error al generar el JSON de productos", e);
+            return Response.status(Status.INTERNAL_SERVER_ERROR)
+                    .entity("{\"error\":\"Error interno del servidor al generar el JSON\"}")
+                    .build();
+        }
     }
 
-    private String productListAsJson() {
+    private String productListAsJson() throws Exception {
         JsonArrayBuilder jsonArrayBuilder = Json.createArrayBuilder();
 
-        for (Product product : productBean.getProducts()) {
-            JsonObjectBuilder jsonObjectBuilder = Json.createObjectBuilder()
-                    .add("name", product.getName())
-                    .add("description", product.getDescription())
-                    .add("price", product.getPrice())
-                    .add("imageUrl", product.getImageUrl());
+        try {
+            for (Product product : productBean.getProducts()) {
+                JsonObjectBuilder jsonObjectBuilder = Json.createObjectBuilder()
+                        .add("name", product.getName())
+                        .add("description", product.getDescription())
+                        .add("price", product.getPrice())
+                        .add("imageUrl", product.getImageUrl());
 
-            jsonArrayBuilder.add(jsonObjectBuilder);
+                jsonArrayBuilder.add(jsonObjectBuilder);
+            }
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "Error al procesar la lista de productos", e);
+            throw new Exception("Error al procesar la lista de productos", e);
         }
 
         return jsonArrayBuilder.build().toString();
